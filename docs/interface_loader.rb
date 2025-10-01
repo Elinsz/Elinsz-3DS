91b879229349c852495c2512de7099efc49b5cd6
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-10-01 00:13:42 -0300

    cmd12

--- docs/interface_loader.rb
+++ docs/interface_loader.rb
@@ -6,13 +6,49 @@ module EIS_Extensions
         File.join(base_dir, "ui", "interface.html")
       end
 
+      def self.html_path
+        base_dir = File.dirname(__FILE__).gsub(%r{//}) { "/" }
+        File.join(base_dir, "Sem uso ext","SKUI","html","elinsz_pt-BR.html")
+      end
+
       def self.css_path
         File.join(File.dirname(__FILE__), "css", "interface.css")
+      end
+
+            def self.css_path
+        File.join(File.dirname(__FILE__), "css", "style_md-tst2.css")
       end
 
       def self.js_path
         File.join(File.dirname(__FILE__), "js", "interface.js")
       end
+
+
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
+      # def self.js_path
+      #   File.join(File.dirname(__FILE__), "js", "interface.js")
+      # end
+
     end
   end
 end
